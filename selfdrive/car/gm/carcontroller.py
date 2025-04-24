e03bf402d5c2a2e02a351a45df9ddab337294763
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-04-24 18:31:19 -0500

    Update carcontroller.py
    (cherry picked from commit a8136e2177a5bef5a056879658b2d2ffa0a0d92e)

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -97,10 +97,6 @@ class CarController(CarControllerBase):
     else:
       pedal_gas = clip((pedaloffset + accel * 0.6), 0.0, 1.0)
 
-    zero = 0.15625  # 40 / 256
-    if accel > 0.:
-      # Scales the accel from 0–1 to 0.156–1
-      pedal_gas = clip(((1 - zero) * pedal_gas + zero), 0., 1.)
 
     pedal_gas = min(pedal_gas, 1.0)
 
